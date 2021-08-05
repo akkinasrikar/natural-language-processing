37cf78af946140c4f9dbd02bb9c52a73f02dbf33
akkinasrikar <akkinasrikar@gmail.com>
2021-08-05 18:29:41 +0530

    nlp

--- Classification vector spaces in nlp/Sentiment analysis with logistic regression/utils.py
+++ Classification vector spaces in nlp/Sentiment analysis with logistic regression/utils.py
@@ -13,8 +13,7 @@ def process_tweets(tweet):
 	tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
 	tweet = re.sub(r'#', '', tweet)
 	tokenizer = TweetTokenizer(preserve_case=False, 
-							   strip_handles=True,
-	                           reduce_len=True)
+							   strip_handles=True,reduce_len=True)
 	tweet_tokens = tokenizer.tokenize(tweet)
 	tweet_clean=list(set([stemmer.stem(i) for i in tweet_tokens if (i not in stopwords_english and i not in string.punctuation)]))
 	return tweet_clean
